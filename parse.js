0011fce35830fe49e9ccbc5caf01d73af0e41686
Ayla Myers <ayla@etsy.com>
2017-09-04 11:27:39 -0400

    Refactored a small amount, also correclt parsing casing

--- parse.js
+++ parse.js
@@ -27,6 +27,60 @@ function determinePronoun(word, form) {
 	return null;
 }
 
+function determineCasing(word) {
+	var text = word.text;
+	var isLowerCase = (text === text.toLowerCase());
+	var isUpperCase = (text[0] === text[0].toUpperCase() && text.substr(1) === text.substr(1).toLowerCase());
+	if (text[0] === "@" && word.pos === "PROPN") {
+		isUpperCase = (text[1] === text[1].toUpperCase() && text.substr(2) === text.substr(2).toLowerCase());
+	}
+	var isAllCaps = (text === text.toUpperCase());
+	if (word.isPunctuation) {
+		return "ignore";
+	}
+	else if (isUpperCase && isAllCaps) { // single character words
+		return "ambiguous";
+	}
+	else if (isLowerCase && (text === "i" || word.pos === "PROPN")) {
+		return "lowercase";
+	}
+	else if (isUpperCase && (word.index === 0 || word.pos !== "PROPN")) {
+		return "uppercase";
+	}
+	else if (isAllCaps) {
+		return "caps";
+	}
+	else if (!isLowerCase && !isUpperCase) {
+		return "unique";
+	}
+	else {
+		return "natural";
+	}
+}
+
+function resolveAmbiguousCasing(word, words) {
+	var nextWord = words[word.index + 1] || null;
+	var prevWord = words[word.index - 1] || null;
+	if (nextWord && nextWord.casing === "caps") {
+		return "caps";
+	}
+	else if (nextWord && nextWord.casing === "unique") {
+		return "unique";
+	}
+	else if (nextWord && nextWord.casing !== "ignore") {
+		return "uppercase";
+	}
+	else if (prevWord && prevWord.casing === "caps") {
+		return "caps";
+	}
+	else if (prevWord && prevWord.casing === "unique") {
+		return "unique";
+	}
+	else {
+		return "uppercase";
+	}
+}
+
 function cloneShallow(obj) {
 	var clone = {};
 	for (var k in obj) {
@@ -36,10 +90,84 @@ function cloneShallow(obj) {
 }
 
 module.exports = function parse(input) {
-	// add pronouns
+	var s, w, word;
+	var casingPerSentence = [];
+	var pronounsPerSentence = [];
+	for (s = 0; s < input.sentences.length; s++) {
+		var sentence = input.sentences[s];
+		var words = [];
+
+		// clone
+		for (w = 0; w < sentence.words.length; w++) {
+			words.push(cloneShallow(sentence.words[w]));
+		}
+
+		// parse
+		var casing = [];
+		var pronouns = [];
+		for (w = 0; w < words.length; w++) {
+			word = words[w];
+
+			// add casing
+			word.casing = determineCasing(word);
+
+			// add pronouns
+			var form = determinePronounForm(word);
+			if (form) {
+				word.isPronoun = true;
+				word.pronoun = determinePronoun(word, form);
+				word.pronounForm = form;
+				pronouns.push({ index: word.index, pronoun: word.pronoun, form: word.pronounForm });
+			}
+			else {
+				word.isPronoun = false;
+			}
+		}
+		for (w = 0; w < words.length; w++) {
+			word = words[w];
+
+			// resolve ambiguous casing
+			if (word.casing === "ambiguous") {
+				word.casing = resolveAmbiguousCasing(word, words);
+			}
+			casing.push(word.casing);
+		}
+		casingPerSentence.push(casing);
+		pronounsPerSentence.push(pronouns);
+	}
+	return {
+		casing: casingPerSentence,
+		pronouns: pronounsPerSentence
+	};
+	/*var i;
+	var words = [];
+	var pronouns = [];
+	// clone
+	for (i = 0; i < input.parsed.words.length; i++) {
+		words.push(cloneShallow(input.parsed.words[i]));
+	}
+	for (i = 0; i < words.length; i++) {
+		var word = words[i];
+		// add pronouns
+		var form = determinePronounForm(word);
+		if (form) {
+			word.isPronoun = true;
+			word.pronoun = determinePronoun(word, form);
+			word.pronounForm = form;
+			pronouns.push({ index: word.index, pronoun: word.pronoun, form: word.pronounForm });
+		}
+		else {
+			word.isPronoun = false;
+		}
+	}*/
+
+	/*var casing = [];
 	var pronouns = [];
 	for (var i = 0; i < input.parsed.words.length; i++) {
 		var word = input.parsed.words[i];
+		// add casing
+		casing.push(determineCasing(word));
+		// add pronouns
 		var form = determinePronounForm(word);
 		if (form) {
 			var pronoun = determinePronoun(word, form);
@@ -49,13 +177,16 @@ module.exports = function parse(input) {
 				form: form
 			});
 		}
-	}
+	}*/
 	return {
-		"textSpeaker": null,
-		"textListener": null,
-		"mentioned": [],
-		"textPerMentioned": {},
-		"verbs": [],
-		"pronouns": pronouns
+		words: [], //words,
+		pronouns: [], //pronouns
+		// "textSpeaker": null,
+		// "textListener": null,
+		// "mentioned": [],
+		// "textPerMentioned": {},
+		// "verbs": [],
+		// "casing": casing,
+		// "pronouns": pronouns
 	};
 };
--- test.js
+++ test.js
@@ -1,34 +1,41 @@
 var data = require('./data');
 var parse = require('./parse');
 
-var actorsByName = {};
-for (var i = 0; i < data.actors.length; i++) {
-	actorsByName[data.actors[i].name] = data.actors[i];
-}
-var actions = data.actions;
+var testCases = data.testCases;
 
 console.log('\n\n');
-for (var i = 0; i < actions.length; i++) {
-	var input = actions[i];
-	console.log('Testing: "' + input.text + '"');
-	var output = parse(input);
-	var correctPronouns = true;
-	if (JSON.stringify(input.pronouns) === JSON.stringify(output.pronouns)) {
+for (var t = 0; t < testCases.length; t++) {
+	var testCase = testCases[t];
+	console.log('Testing: "' + testCase.text + '"');
+
+	// test pronouns
+	var output = parse(testCase.input);
+	if (JSON.stringify(testCase.pronouns) === JSON.stringify(output.pronouns)) {
 		console.log('  pronouns:    success');
 	}
 	else {
 		console.log('  pronouns:    FAILURE!!');
-		console.log('    Input:  ' + JSON.stringify(input.pronouns));
-		console.log('    Output: ' + JSON.stringify(output.pronouns));
+		console.log('       input:  ' + JSON.stringify(testCase.pronouns));
+		console.log('       output: ' + JSON.stringify(output.pronouns));
 	}
-	/*
-	if (input.textSpeaker === output.textSpeaker) {
+
+	// test casing
+	if (JSON.stringify(testCase.casing) === JSON.stringify(output.casing)) {
+		console.log('  casing:      success');
+	}
+	else {
+		console.log('  casing:      FAILURE!!');
+		console.log('       input:  ' + JSON.stringify(testCase.casing));
+		console.log('       output: ' + JSON.stringify(output.casing));
+	}
+
+	/*if (input.textSpeaker === output.textSpeaker) {
 		console.log('  textSpeaker: success');
 	}
 	else {
 		console.log('  textSpeaker: FAILURE!!');
-		console.log('    Input:  ' + input.textSpeaker);
-		console.log('    Output: ' + output.textSpeaker);
+		console.log('    input:  ' + input.textSpeaker);
+		console.log('    output: ' + output.textSpeaker);
 	}
 	*/
 }
